f53325346f768b374011df740d88a601b5af6276
Germano Gualberto <gmanopoliano@gmail.com>
2023-02-01 11:57:43 -0300

    button to start quiz created

--- quiz-app-react/src/App.js
+++ quiz-app-react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Header from "./Header";
-import QuizSection from "./QuizSection";
+import Header from "./components/Header";
+import QuizSection from "./components/QuizSection";
 
 class App extends Component {
   render() {
---
+++ quiz-app-react/src/components/Quiz.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Quiz extends Component {
+  render() {
+    return <p>This is a Quiz</p>;
+  }
+}
+
+export default Quiz;
